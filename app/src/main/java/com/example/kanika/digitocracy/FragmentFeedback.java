8e0c118c2e54ff9f7f834e58d27a794cee037f67
Vethics_android <41325213+VethicsGit@users.noreply.github.com>
2018-08-18 14:27:18 +0530

    feddback

--- app/src/main/java/com/example/kanika/digitocracy/FragmentFeedback.java
+++ app/src/main/java/com/example/kanika/digitocracy/FragmentFeedback.java
@@ -7,13 +7,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FragmentFeedback extends Fragment {
 
 
+    EditText feedback_subject,feedback_phone,feedback_message;
+    Button feedback_submit;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.feedback.,container,false);
+        View view=inflater.inflate(R.layout.feedback,container,false);
+
+        feedback_subject=view.findViewById(R.id.feedback_subject);
+        feedback_message=view.findViewById(R.id.feedback_meassage);
+        feedback_phone=view.findViewById(R.id.feedback_phone);
+        feedback_submit=view.findViewById(R.id.feedback_submit);
+
+        return view;
     }
 }
