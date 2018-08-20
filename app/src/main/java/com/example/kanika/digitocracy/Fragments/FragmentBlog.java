6898295cb7c040aabd921ffc2db9112b2014f491
VethicsGit_android <41325213+VethicsGit@users.noreply.github.com>
2018-08-20 10:37:22 +0530

    pagination in blogs section

--- app/src/main/java/com/example/kanika/digitocracy/Fragments/FragmentBlog.java
+++ app/src/main/java/com/example/kanika/digitocracy/Fragments/FragmentBlog.java
@@ -3,7 +3,6 @@ package com.example.kanika.digitocracy.Fragments;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Movie;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -22,9 +21,7 @@ import com.example.kanika.digitocracy.APISHelper.APIS;
 import com.example.kanika.digitocracy.Adapter.BlogAdapter;
 import com.example.kanika.digitocracy.R;
 import com.example.kanika.digitocracy.extra.PaginationScrollListener;
-import com.example.kanika.digitocracy.extra.Paginator;
 import com.example.kanika.digitocracy.extra.RecyclerViewMargin;
-import com.srx.widget.PullToLoadView;
 
 import java.util.List;
 
@@ -38,9 +35,6 @@ public class FragmentBlog extends Fragment {
     SharedPreferences LoginPref;
 
     int offset = 0;
-    int blog_category_id = 0;
-    int pos= 0;
-    boolean isLoading = false;
     BlogAdapter blogAdapter;
 
     @Nullable
@@ -51,14 +45,10 @@ public class FragmentBlog extends Fragment {
 
         final LinearLayoutManager manager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
         blog_recyclar_view.setLayoutManager(manager);
-        /*final BlogAdapter blogAdapter = new BlogAdapter(new ArrayList<BlogList>(), getContext());
-        blog_recyclar_view.setAdapter(blogAdapter);
-*/
 
-        /*
         if (offset==0) {
             RecyclerViewMargin decoration = new RecyclerViewMargin(10, 2);
-            blog_recyclar_view.getRecyclerView().addItemDecoration(decoration);
+            blog_recyclar_view.addItemDecoration(decoration);
 
             LoginPref = getContext().getSharedPreferences("LoginStatus", Context.MODE_PRIVATE);
             final ProgressDialog mProgressDialog = new ProgressDialog(getContext());
@@ -84,7 +74,7 @@ public class FragmentBlog extends Fragment {
 
                             List<BlogList> blogLists = re.getBlogList();
                             blogAdapter = new BlogAdapter(blogLists, getContext());
-                            blog_recyclar_view.getRecyclerView().setAdapter(blogAdapter);
+                            blog_recyclar_view.setAdapter(blogAdapter);
                         } else {
                             mProgressDialog.dismiss();
                         }
@@ -97,75 +87,34 @@ public class FragmentBlog extends Fragment {
                     Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
                 }
             });
-        }else*/
+        }
 
-
-//        new Paginator(getContext(),blog_recyclar_view);
-
-
-//        ServiceCall();
-
-       /* blog_recyclar_view.setOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-
-            }
-
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                int visibleItemCount = manager.getChildCount();
-                int totalItemCount = manager.getItemCount();
-                int pastVisibleItems = manager.findFirstVisibleItemPosition();
-
-                pos=totalItemCount;
-                if (visibleItemCount + pastVisibleItems >= totalItemCount && pastVisibleItems >= 0) {
-                    ServiceCall();
-                }
-            }
-        });*/
-
-        /*blog_recyclar_view.addOnScrollListener(new PaginationScrollListener(manager) {
+        blog_recyclar_view.addOnScrollListener(new PaginationScrollListener(manager,getContext()) {
             @Override
             protected void loadMoreItems() {
-                isLoading = true;
-                currentPage += 1; //Increment page index to load the next one
-                loadNextPage();
+                ServiceCall();
             }
 
             @Override
             public int getTotalPageCount() {
-                return TOTAL_PAGES;
+                return 0;
             }
 
             @Override
             public boolean isLastPage() {
-                return isLastPage;
+                return false;
             }
 
             @Override
             public boolean isLoading() {
-                return isLoading;
+                return false;
             }
         });
 
-        loadFirstPage();*/
         return view;
     }
 
- /*   private void loadFirstPage() {
-        // fetching dummy data
-        List<Movie> movies = Movie.createMovies(adapter.getItemCount());
-        progressBar.setVisibility(View.GONE);
-        adapter.addAll(movies);
-
-        if (currentPage <= TOTAL_PAGES) adapter.addLoadingFooter();
-        else isLastPage = true;
-    }
-*/
     public void ServiceCall() {
-//        final BlogAdapter blogAdapter = new BlogAdapter();
 
         LoginPref = getContext().getSharedPreferences("LoginStatus", Context.MODE_PRIVATE);
 
@@ -182,12 +131,9 @@ public class FragmentBlog extends Fragment {
                     if (re.getStatus().equals("true")) {
                         offset = re.getOffset();
                         List<BlogList> blogLists = re.getBlogList();
-//                        int pos=blogAdapter.getItemCount();
                         for (int x = 0; x < blogLists.size(); x++)
                             blogAdapter.add(blogLists.get(x));
                         blogAdapter.notifyDataSetChanged();
-//                        blog_recyclar_view.scrollToPosition(pos);
-//                        blog_recyclar_view.setAdapter(blogAdapter);
                     }else {
                         Toast.makeText(getContext(), "This is the end", Toast.LENGTH_SHORT).show();
                     }
--- app/src/main/java/com/example/kanika/digitocracy/extra/PaginationScrollListener.java
+++ app/src/main/java/com/example/kanika/digitocracy/extra/PaginationScrollListener.java
@@ -1,14 +1,32 @@
 package com.example.kanika.digitocracy.extra;
 
+import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.example.kanika.digitocracy.APIResponse.BlogList.BlogList;
+import com.example.kanika.digitocracy.APIResponse.BlogList.BlogListResponse;
+import com.example.kanika.digitocracy.APISHelper.API;
+import com.example.kanika.digitocracy.APISHelper.APIS;
+import com.example.kanika.digitocracy.Adapter.BlogAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public abstract class PaginationScrollListener extends RecyclerView.OnScrollListener {
 
     LinearLayoutManager layoutManager;
+    Context context;
+    BlogAdapter blogAdapter;
 
-    public PaginationScrollListener(LinearLayoutManager layoutManager) {
+    public PaginationScrollListener(LinearLayoutManager layoutManager,Context context) {
         this.layoutManager = layoutManager;
+        this.context=context;
     }
 
     @Override
@@ -23,15 +41,59 @@ public abstract class PaginationScrollListener extends RecyclerView.OnScrollList
             if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount
                     && firstVisibleItemPosition >= 0) {
                 loadMoreItems();
+//                blogAdapter = new BlogAdapter(new ArrayList<BlogList>(), context);
+
+//                ServiceCall();
             }
         }
     }
 
-    protected abstract void loadMoreItems();
+    protected abstract void loadMoreItems(
+
+    );
 
     public abstract int getTotalPageCount();
 
     public abstract boolean isLastPage();
 
     public abstract boolean isLoading();
+
+    /*BlogAdapter blogAdapter;
+    public void ServiceCall() {
+//        final BlogAdapter blogAdapter = new BlogAdapter();
+
+        LoginPref = context.getSharedPreferences("LoginStatus", Context.MODE_PRIVATE);
+
+        API apiService = APIS.getRetrofit().create(API.class);
+        Call<BlogListResponse> call1 = apiService.blog_list(LoginPref.getString("user_id", ""), offset, "", LoginPref.getString("token", ""));
+
+        call1.enqueue(new Callback<BlogListResponse>() {
+            @Override
+            public void onResponse(Call<BlogListResponse> call, Response<BlogListResponse> response) {
+                BlogListResponse blogListResponse = response.body();
+                List<com.example.kanika.digitocracy.APIResponse.BlogList.Response> resList = blogListResponse.getResponse();
+                for (int i = 0; i < resList.size(); i++) {
+                    com.example.kanika.digitocracy.APIResponse.BlogList.Response re = resList.get(i);
+                    if (re.getStatus().equals("true")) {
+                        offset = re.getOffset();
+                        List<BlogList> blogLists = re.getBlogList();
+//                        int pos=blogAdapter.getItemCount();
+                        for (int x = 0; x < blogLists.size(); x++)
+                            blogAdapter.add(blogLists.get(x));
+                        blogAdapter.notifyDataSetChanged();
+//                        blog_recyclar_view.scrollToPosition(pos);
+//                        blog_recyclar_view.setAdapter(blogAdapter);
+                    }else {
+                        Toast.makeText(context, "This is the end", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BlogListResponse> call, Throwable t) {
+
+            }
+        });
+    }*/
 }
