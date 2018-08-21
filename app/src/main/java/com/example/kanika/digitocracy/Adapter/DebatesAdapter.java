c657d7761c8bc214ad013044ff5db260b41216bc
VethicsGit_android <41325213+VethicsGit@users.noreply.github.com>
2018-08-21 17:25:48 +0530

    debates adapter class

--- app/src/main/java/com/example/kanika/digitocracy/Adapter/DebatesAdapter.java
+++ app/src/main/java/com/example/kanika/digitocracy/Adapter/DebatesAdapter.java
@@ -16,13 +16,20 @@ import com.example.kanika.digitocracy.APIResponse.DebatesList.DebateParticipant;
 import com.example.kanika.digitocracy.Public;
 import com.example.kanika.digitocracy.R;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class DebatesAdapter extends RecyclerView.Adapter<DebatesAdapter.ViewHolder> {
 
     List<DebateList>debateList;
     Context context;
+
+    String day[]=new String[]{"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
 
 
     public DebatesAdapter(List<DebateList> debateList, Context context) {
@@ -36,7 +43,7 @@ public class DebatesAdapter extends RecyclerView.Adapter<DebatesAdapter.ViewHold
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view= LayoutInflater.from(context).inflate(R.layout.fragment_debates,null);
+        View view= LayoutInflater.from(context).inflate(R.layout.debates_layout,null);
 
         return new ViewHolder(view);
     }
@@ -46,9 +53,24 @@ public class DebatesAdapter extends RecyclerView.Adapter<DebatesAdapter.ViewHold
 
         DebateList debate=debateList.get(position);
         holder.debates_title.setText(debate.getTopicTitle());
-        holder.debates_date_time.setText(debate.getScheduleDateTime());
 
-     List<DebateParticipant>debateParticipants =debate.getDebateParticipants();
+
+        Date date = new Date(debate.getScheduleDateTime());
+
+        android.text.format.DateFormat.format("yyyy-MM-dd hh:mm:ss a", date);
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(date); // yourdate is an object of type Date
+
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy kk:mm p");
+        try {
+            Date date1 = format.parse(debate.getScheduleDateTime());
+            holder.debates_date_time.setText(day[dayOfWeek]+", "+date1.toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        List<DebateParticipant>debateParticipants =debate.getDebateParticipants();
      for (int i=0;i<debateParticipants.size();i++){
          DebateParticipant debat=debateParticipants.get(position);
          holder.debates_profilename1.setText(debat.getName());
